5de692ad51293c3f04bdb0b6155a550379c30375
Flora Mae Requillo <requillofloramae@gmail.com>
2024-12-02 21:25:57 +0800

    message

--- src/app/helper/upload.ts
+++ src/app/helper/upload.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 import { v4 as uuidv4 } from 'uuid';
 // Initialize Supabase client
-const supabaseUrl = 'https://brtsckbiqiwmrqcjqmfm.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJydHNja2JpcWl3bXJxY2pxbWZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjEwMjkxNzYsImV4cCI6MjAzNjYwNTE3Nn0.WY1vRPLeAd7XH5EkD_8KemcKJXvG_-LznucJHxkIgGw';
+const supabaseUrl = 'https://dbtzyebosltbhukbgmgt.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRidHp5ZWJvc2x0Ymh1a2JnbWd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzMxNDI1NDUsImV4cCI6MjA0ODcxODU0NX0.6yWIxeQAdavPpfiG4HlXoOcv2XjSwQ5-Kthng9FWZbM';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export async function uploadImage(file:File) {
